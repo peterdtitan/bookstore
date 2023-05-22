ab50ab97ba4c519143de976ffa0469295a009c0f
Peter Okorafor <peterdtitan@gmail.com>
2023-05-22 23:27:11 +0300

    Fix AddBook component responsiveness

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -23,11 +23,11 @@ export default function AddBook() {
   };
 
   return (
-    <div className="border-t-[0.5px] border-slate-400 py-8 mt-10 flex flex-col">
+    <div className="border-t-[0.5px] border-slate-400 p-4 mt-10 flex flex-col">
       <p className="text-[#888888] text-xl font-montserrat font-semibold">ADD NEW BOOK</p>
-      <form className="flex w-full justify-between items-center mt-6" id="add-book">
-        <input className="w-[55%] h-10 p-2 border-[0.5px] border-slate-300 rounded-sm focus:outline-none" placeholder="Book" value={title} onChange={(e) => setTitle(e.target.value)} />
-        <input className="w-[25%] h-10 p-2 border-[0.5px] border-slate-300 rounded-sm focus:outline-none" placeholder="Author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+      <form className="flex flex-col md:flex-row gap-y-4 w-full justify-between md:items-center mt-6" id="add-book">
+        <input className="md:w-[55%] h-10 p-2 border-[0.5px] border-slate-300 rounded-sm focus:outline-none" placeholder="Book" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <input className="md:w-[25%] h-10 p-2 border-[0.5px] border-slate-300 rounded-sm focus:outline-none" placeholder="Author" value={author} onChange={(e) => setAuthor(e.target.value)} />
         <button className="bg-blue-500 text-white rounded-md px-4 py-2 font-normal" type="submit" onClick={(e) => handleSubmit(e)}>ADD BOOK</button>
       </form>
     </div>
