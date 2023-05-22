ddbe76162283fdb84bb2f06eccfdc79004b094d5
Peter Okorafor <peterdtitan@gmail.com>
2023-05-22 23:58:08 +0300

    Fix outside click for user menu

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Categories() {
   return (
-    <div className="mt-4">
+    <div className="mt-4 p-2">
       <button className="bg-blue-500 text-white rounded-md px-4 py-2 font-normal" type="button">CHECK STATUS</button>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FaUserAlt } from 'react-icons/fa';
 import { AiOutlineMenu } from 'react-icons/ai';
 
 export default function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  useEffect(() => {
+    const handler = () => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener('mousedown', handler);
+  });
+
   return (
     <div className="h-20 w-full flex justify-between items-center bg-white p-10 border-b-[0.5px] border-slate-400">
       <div className="flex justify-between items-center gap-16">
@@ -17,7 +27,18 @@ export default function Navbar() {
         <FaUserAlt className="text-lg text-blue-500" />
       </div>
       <div className="md:hidden">
-        <AiOutlineMenu className="text-2xl text-blue-500" />
+        <button type="button" aria-label="Menu" onClick={() => setShowMenu(!showMenu)}><AiOutlineMenu className="text-2xl text-blue-500" /></button>
+        {showMenu && (
+          <div className="absolute top-20 right-0 w-40 p-2 bg-white rounded-md shadow-md">
+            <div className="flex flex-col justify-between items-center gap-6 p-4">
+              <div className="rounded-full border-[0.5px] border-slate-300 p-3">
+                <FaUserAlt className="text-lg text-blue-500" />
+              </div>
+              <Link to="/" className="text-sm font-medium font-montserrat text-black">BOOKS</Link>
+              <Link to="/categories" className="text-sm font-medium font-montserrat text-gray-400">CATEGORIES</Link>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
